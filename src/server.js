93295bce06892721cf4f83549a967b5304424435
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-07-03 17:24:40 +0530

    Code Refactoring

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
+require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
-const path = require("path")
+// const path = require("path")
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser : true, useUnifiedTopology: true})
 
 const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
+
 
 const db = mongoose.connection
 db.on("error", ()=>console.log(err))
 db.once("open", ()=>console.log("Connection to monogDB successful"))
 
 const userRoute =  require("./routers/user")
+const storeRoute = require("./routers/store")
 
 app.use("/users", userRoute)
+app.use("/store", storeRoute)
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`))
